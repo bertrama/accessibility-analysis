31d7f5e9df68fa31ee9e9a682ffbe3e656bd4568
Albert Bertram <bertrama@umich.edu>
2017-11-04 11:52:58 -0400

    Add element model.

---
+++ db/migrate/20171104154327_create_elements.rb
@@ -0,0 +1,22 @@
+class CreateElements < ActiveRecord::Migration[5.1]
+  def change
+    create_table :elements do |t|
+      t.string :visibility
+      t.string :display
+      t.string :tag
+      t.string :path
+      t.text :html
+      t.integer :top
+      t.integer :left
+      t.integer :bottom
+      t.integer :right
+      t.integer :x
+      t.integer :y
+      t.integer :width
+      t.integer :height
+      t.belongs_to :messages
+
+      t.timestamps
+    end
+  end
+end
