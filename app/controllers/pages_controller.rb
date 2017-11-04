2efbb42e694e976a0b1e88f934da810c91a318aa
Albert Bertram <bertrama@umich.edu>
2017-11-04 09:57:23 -0400

    Have a working create/update/show.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,36 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end
+
+  def new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    @page.save
+    redirect_to @page
+  end
+
+  def show
+    @page = page_id
+  end
+
+  def edit
+    @page = page_id
+  end
+
+  def update
+    @page = page_id
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  private
+  def page_params
+    params.require(:page).permit(:url)
+  end
+
+  def page_id
+    Page.find(params[:id])
+  end
 end
